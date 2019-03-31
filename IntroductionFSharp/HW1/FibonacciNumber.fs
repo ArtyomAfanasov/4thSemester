d01be3e8f0fb20a4433e046b45251cf67480bd6a
ArtyomAfanasov <artikul396@mail.ru>
2019-03-31 19:33:34 +0300

    Добавил факториал. Добавил числа Фибоначчи.

---
+++ IntroductionFSharp/HW1/FibonacciNumber.fs
@@ -0,0 +1,21 @@
+﻿/// Модуль подсчёта чисел Фибоначи за линейное время
+module FibonacciNumber
+
+    /// Подсчёт числа Фибоначчи с номером 'input'.
+    let fibonacciNumber inputNumber =                  
+        let mutable operationOne = (+)
+        let mutable operationTwo = (-)
+        if inputNumber < 0 then 
+            operationOne <- (-)
+            operationTwo <- (+)                            
+
+        let rec loop curNumberOfNumber throughOneBeforeCurNumber previouseNumber =            
+            match curNumberOfNumber with
+            | positive when positive <> 0 ->                                   
+                let curNumber = operationOne throughOneBeforeCurNumber previouseNumber                                   
+                loop (operationTwo curNumberOfNumber 1) previouseNumber curNumber     
+                    
+            | 0 when throughOneBeforeCurNumber <> 0 ->  throughOneBeforeCurNumber
+            | 0 -> throughOneBeforeCurNumber
+
+        loop inputNumber 0 1                      
