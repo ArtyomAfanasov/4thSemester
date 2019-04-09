a75b0b31a089c1b08a27ef356bc852dea696bdd4
ArtyomAfanasov <artikul396@mail.ru>
2019-04-09 10:47:50 +0300

    Добавил поиск первого вхождения числа в список.

---
+++ IntroductionFSharp/HW2/FirstEntry.fs
@@ -0,0 +1,15 @@
+﻿module FirstEntry
+
+    let firstEntry list wishfulNumber =
+        let rec loop tail =
+            match tail with
+            | head :: tail -> 
+                if head = wishfulNumber 
+                    then List.length list - (List.length tail + 1)
+                else 
+                    loop tail
+
+            // Заданного числа в массиве нет.
+            | _ -> -1 
+        
+        loop list
