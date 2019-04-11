169ef9b2ee6add8fbdcd24b75b1f1c4fd3eb6e0e
ArtyomAfanasov <artikul396@mail.ru>
2019-04-11 16:57:43 +0300

    Добавил  + тесты. CI

---
+++ IntroductionFSharp/Hw2/Hw2.Tests/FirstEntryTests.fs
@@ -0,0 +1,28 @@
+namespace Hw2.Tests
+
+open NUnit.Framework
+open FsUnit
+open FirstEntry
+
+[<TestFixture>]
+type TestClass () =
+    
+    [<Test>]
+    member this.``FirstEntry should complete with 4 for data: 5 in [1;2;3;4;5;6]`` () =
+        firstEntry [1;2;3;4;5;6] 5 |> should equal 4
+    
+    [<Test>]
+    member this.``FirstEntry should complete with 0 for data: 5 in [5;4;3]`` () =
+        firstEntry [5;4;3] 5 |> should equal 0
+    
+    [<Test>]
+    member this.``FirstEntry should complete with 5 for data: 5 in [0;1;2;3;4;5]`` () = 
+        firstEntry [0;1;2;3;4;5] 5 |> should equal 5
+    
+    [<Test>]
+    member this.``FirstEntry should complete with exception for data: 5 in []`` () =
+        (fun () -> firstEntry [] 5 |> ignore) |> should throw typeof<System.Exception> 
+    
+    [<Test>]
+    member this.``FirstEntry should complete with exception for data: 5 in [1;2;3]`` () =
+        (fun () -> firstEntry [1;2;3] 5 |> ignore) |> should throw typeof<System.Exception>   
---
+++ IntroductionFSharp/Hw2/Hw2/FirstEntry.fs
@@ -0,0 +1,20 @@
+﻿module FirstEntry
+
+/// Вернуть номер первого вхождения элемента в список.
+let firstEntry list wishfulElement =
+    let rec loop tail =
+        match tail with
+        | head :: tail -> 
+            if head = wishfulElement    
+                then List.length list - (List.length tail + 1)
+            else 
+                loop tail
+
+        // Заданного числа в массиве нет.
+        | _ -> raise (System.Exception("Wishful number don't consist in list"))            
+        
+    loop list
+
+[<EntryPoint>]
+let main argv =    
+    0 
