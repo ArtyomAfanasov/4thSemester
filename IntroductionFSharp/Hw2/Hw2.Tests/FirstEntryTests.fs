feb49a7ed3359202d933d2aa5bade873a6ef35b9
ArtyomAfanasov <artikul396@mail.ru>
2019-04-11 17:02:54 +0300

    Стайлгайд. Примечание: предыдущий коммит - это задача

--- IntroductionFSharp/Hw2/Hw2.Tests/FirstEntryTests.fs
+++ IntroductionFSharp/Hw2/Hw2.Tests/FirstEntryTests.fs
@@ -8,21 +8,21 @@ open FirstEntry
 type TestClass () =
     
     [<Test>]
-    member this.``FirstEntry should complete with 4 for data: 5 in [1;2;3;4;5;6]`` () =
-        firstEntry [1;2;3;4;5;6] 5 |> should equal 4
+    member this.``FirstEntry should complete with 4 for data: 5 in [ 1; 2; 3; 4; 5; 6 ]`` () =
+        firstEntry [ 1; 2; 3; 4; 5; 6 ] 5 |> should equal 4
     
     [<Test>]
-    member this.``FirstEntry should complete with 0 for data: 5 in [5;4;3]`` () =
-        firstEntry [5;4;3] 5 |> should equal 0
+    member this.``FirstEntry should complete with 0 for data: 5 in [ 5; 4; 3 ]`` () =
+        firstEntry [ 5; 4; 3 ] 5 |> should equal 0
     
     [<Test>]
-    member this.``FirstEntry should complete with 5 for data: 5 in [0;1;2;3;4;5]`` () = 
-        firstEntry [0;1;2;3;4;5] 5 |> should equal 5
+    member this.``FirstEntry should complete with 5 for data: 5 in [ 0; 1; 2; 3; 4; 5 ]`` () = 
+        firstEntry [ 0; 1; 2; 3; 4; 5 ] 5 |> should equal 5
     
     [<Test>]
     member this.``FirstEntry should complete with exception for data: 5 in []`` () =
         (fun () -> firstEntry [] 5 |> ignore) |> should throw typeof<System.Exception> 
     
     [<Test>]
-    member this.``FirstEntry should complete with exception for data: 5 in [1;2;3]`` () =
-        (fun () -> firstEntry [1;2;3] 5 |> ignore) |> should throw typeof<System.Exception>   
+    member this.``FirstEntry should complete with exception for data: 5 in [ 1; 2; 3 ]`` () =
+        (fun () -> firstEntry [ 1; 2; 3 ] 5 |> ignore) |> should throw typeof<System.Exception>   
