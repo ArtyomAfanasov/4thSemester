12112d23b5e1f94b7a94919b3fb2b7b3d4fb7a16
ArtyomAfanasov <artikul396@mail.ru>
2019-04-12 17:06:53 +0300

    черновик

---
+++ IntroductionFSharp/Hw3/Hw3.Tests/ForTest1Tests.fs
@@ -0,0 +1,22 @@
+﻿namespace Hw3.Tests
+
+open NUnit.Framework
+open FsUnit
+open Palindrome
+
+[<TestFixture>]
+type ForTest1TestClass () =
+
+    [<Test>]
+    member this.``t`` () =
+        (maxPalindrome ()).Value |> should equal 906609
+
+    [<Test>]
+    member this.``ss`` () = 
+        isPolindrome 906609 |> should equal true
+
+    [<Test>]
+    member this.``asdasd`` () = 
+        createFlipedListFromStr "12345" |> should equal [ '5'; '4'; '3'; '2'; '1' ]
+
+
---
+++ IntroductionFSharp/Hw3/Hw3/ForTest1.fs
@@ -0,0 +1,59 @@
+﻿module Palindrome
+
+/// Обратить список.
+let listFlip list =        
+    let rec loop listTail accFlipedList =    
+        match listTail with                 
+        | head :: tail -> loop tail (head :: accFlipedList)            
+        | [] -> accFlipedList
+
+    loop list []
+
+/// Создать список из строки.
+let createFlipedListFromStr number =
+    let str = number.ToString()
+    let length = String.length (str)
+    let rec listCreationLoop accList step = 
+        match str.[step] with 
+        | _ when step = length - 1 ->                 
+            str.[step] :: accList
+        | ' ' ->                
+            listCreationLoop accList (step + 1)                               
+        | _  ->                                
+            listCreationLoop (str.[step] :: accList) (step + 1)
+
+    listCreationLoop [] 0                
+        
+/// Проверить: является ли строка палиндромом.
+let isPolindrome number =    
+    //let preparedStr = str.ToLower()
+    let convertedAndFlipedListFromStr = createFlipedListFromStr number         
+    let normalListFromStr = listFlip convertedAndFlipedListFromStr
+
+    normalListFromStr = convertedAndFlipedListFromStr
+
+// Найти максимальный палиндром, полученный произведением двух трёхзначных чисел.
+let maxPalindrome () =   
+    let firstOld = 999
+    let secondOld = 999
+
+    let mutable maxPalindrome = 0
+    
+    let rec loopForFirst first = 
+        match first with
+        | _ when first = -1 -> Some(maxPalindrome)
+        | _ when first = 100 -> None
+        | _ -> loopForSecond first secondOld     
+
+    and loopForSecond first second = 
+        match (first, second) with
+        | a, b when isPolindrome (a * b) ->
+            if a * b < maxPalindrome then 
+                loopForFirst -1
+            else 
+                maxPalindrome <- a * b
+                loopForSecond first (second - 1)
+        | a, b when b = 100 -> loopForFirst (a - 1)
+        | _ -> loopForSecond first (second - 1)
+    
+    loopForFirst firstOld
