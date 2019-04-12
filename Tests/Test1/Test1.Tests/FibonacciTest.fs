c3b7f50e1aa0befdf6608c92b50ff277bd3b2491
ArtyomAfanasov <artikul396@mail.ru>
2019-04-12 18:09:45 +0300

    Фибоначчи.

---
+++ Tests/Test1/Test1.Tests/FibonacciTest.fs
@@ -0,0 +1,27 @@
+namespace Test1.Tests
+
+open FsUnit
+open NUnit.Framework
+open FibonacciNumber
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member this.``SumOfOddFibonacciNumberLessThenSupremum with supremum is 4 should equal 7``() =       
+        SumOfOddFibonacciNumberLessThenSupremumTest 4 |> should equal 2
+
+    [<Test>]
+    member this.``SumOfOddFibonacciNumberLessThenSupremum with supremum is 4 should equal 143``() =       
+        SumOfOddFibonacciNumberLessThenSupremumTest 55 |> should equal 44
+
+    // ( (1 + sqrt(5) )^x - (1 - sqrt(5))^x )/( (2^x) * sqrt(5) ) = 1346269, where x = 31 
+    // ( (1 + sqrt(5) )^x - (1 - sqrt(5))^x )/( (2^x) * sqrt(5) ) = 832040, where x = 30 
+    [<Test>]
+    member this.``lastFibonacciNumberLessThan1000000 should equal 832040``() =       
+        lastFibonacciNumberLessThan1000000 () |> should equal 832040
+
+    // WolframAlpha: sum (( (1 + sqrt(5) )^x - (1 - sqrt(5))^x )/( (2^x) * sqrt(5) )) = 2178308, x = 1 to x = 30
+    [<Test>]
+    member this.``SumOfAllFibonacciNumberLessThenSupremumTest should equal 2178308``() =       
+        SumOfAllFibonacciNumberLessThenSupremumTest 1000000 |> should equal 2178308    
---
+++ Tests/Test1/Test1/Fibonacci.fs
@@ -0,0 +1,57 @@
+﻿module FibonacciNumber
+
+/// Сумма чисел Фибоначчи, не превосходящих 1000000.
+let SumOfOddFibonacciNumberLessThen1000000 () =    
+    let supremum = 1000000
+    let rec loop beforeLastNumber previousNumber acc =            
+        match (beforeLastNumber, previousNumber) with
+        | _ when beforeLastNumber + previousNumber > supremum -> acc             
+        | _ when (beforeLastNumber + previousNumber) % 2 = 0 -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber (acc + curNumber)    
+        | _ -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber acc        
+        
+    loop 0 1 0 
+
+/// Тест. Сумма чётных чисел Фибоначчи, не превосходящих `sup`.
+let SumOfOddFibonacciNumberLessThenSupremumTest supremum =      
+    let rec loop beforeLastNumber previousNumber acc =            
+        match (beforeLastNumber, previousNumber) with
+        | _ when beforeLastNumber + previousNumber > supremum -> acc             
+        | _ when (beforeLastNumber + previousNumber) % 2 = 0 -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber (acc + curNumber)    
+        | _ -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber acc    
+        
+    loop 0 1 0 
+
+/// Тест. Сумма всех чисел Фибоначчи, не превосходящих `sup`.
+let SumOfAllFibonacciNumberLessThenSupremumTest supremum=      
+    let rec loop beforeLastNumber previousNumber acc =            
+        match (beforeLastNumber, previousNumber) with
+        | _ when beforeLastNumber + previousNumber > supremum -> acc             
+        | _  -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber (acc + curNumber)            
+        
+    loop 0 1 1 
+
+/// Тест. Последнее число фибоначи, меньшее 1000000.
+let lastFibonacciNumberLessThan1000000 () = 
+    let supremum = 1000000
+    let rec loop beforeLastNumber previousNumber =            
+        match (beforeLastNumber, previousNumber) with
+        | _ when beforeLastNumber + previousNumber > supremum -> previousNumber             
+        | _ -> 
+            let curNumber = beforeLastNumber + previousNumber                                   
+            loop previousNumber curNumber       
+        
+    loop 0 1
+    
+[<EntryPoint>]
+let main argv =    
+    0 
