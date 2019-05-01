34db828aa2c35e29a8e4139f6f79c0804ffc9964
ArtyomAfanasov <artikul396@mail.ru>
2019-05-01 15:29:38 +0300

    Функция разбиения листа на два листа.

---
+++ IntroductionFSharp/Hw2/Hw2.Tests/MergesortTests.fs
@@ -0,0 +1,30 @@
+﻿namespace Hw2.Tests
+
+open NUnit.Framework
+open FsUnit
+open Mergesort
+
+[<TestFixture>]
+type MergesortTestsClass () =
+
+    [<Test>]
+    member this.``splitOnTwo on data [1; 2; 3; 4] should return ([3; 1], [4; 2])`` () =
+        splitOnTwo [1; 2; 3; 4] |> should equal ([3; 1], [4; 2])
+
+    [<Test>]
+    member this.``splitOnTwo on data  [1; 2; 3; 4; 5] should return ([5; 3; 1], [4; 2]) `` () =
+        splitOnTwo [1; 2; 3; 4; 5] |> should equal ([5; 3; 1], [4; 2]) 
+
+    [<Test>]
+    member this.``splitOnTwo on data [] should return ([], [])`` () =
+        splitOnTwo [] |> should equal ([], []) 
+        
+    [<Test>]
+    member this.``sortAndMergeTwoLists on data [5; 3; 4] [10; 8] should return [3; 4; 5; 8; 10]`` () =
+        sortAndMergeTwoLists [5; 3; 4] [10; 8] |> should equal [3; 4; 5; 8; 10]
+
+    // ===============================================TestTest=======================
+    
+    [<Test>]
+    member this.``Test Test`` () =
+        [1, 2] @ [] |> should equal [1, 2]                     
---
+++ IntroductionFSharp/Hw2/Hw2/Mergesort.fs
@@ -0,0 +1,16 @@
+﻿/// Модуль с функцией "Сортировка слиянием".
+module Mergesort
+
+/// Разделить список на два списка.
+let splitOnTwo list =
+    let rec loop list left right =
+        match list with 
+        | first :: (second :: tail) -> loop tail (first :: left) (second :: right)
+        | head :: tail -> (head :: left, right)
+        | [] -> (left, right)
+
+    loop list [] []    
+
+/// Сортировать и слить два массива.
+let sortAndMergeTwoLists first second =
+    System.NotImplementedException
