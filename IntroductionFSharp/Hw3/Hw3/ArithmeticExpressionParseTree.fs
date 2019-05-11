4863af953f52b7e541a52c39794a264f1d1dfd36
ArtyomAfanasov <artikul396@mail.ru>
2019-05-11 16:38:52 +0300

    Подсчёт значения арифметического дерева разбора ГОТОВО

--- IntroductionFSharp/Hw3/Hw3/ArithmeticExpressionParseTree.fs
+++ IntroductionFSharp/Hw3/Hw3/ArithmeticExpressionParseTree.fs
@@ -3,67 +3,26 @@
 module ArithmeticExpressionParseTree
 
 /// Дерево разбора арифметического выражения.
-type ParseTree =
-    | None
+type ArithmExprTree =   
     | Value of int
-    | Plus of ParseTree * ParseTree
-    | Minus of ParseTree * ParseTree    
-    | Division of ParseTree * ParseTree
-    | Multiply of ParseTree * ParseTree   
-
-type Operation =
-    | NoneOp
-    | PlusOp
-    | MinusOp
-    | DivisionOp
-    | MultiplyOp
+    | Plus of ArithmExprTree * ArithmExprTree
+    | Minus of ArithmExprTree * ArithmExprTree    
+    | Division of ArithmExprTree * ArithmExprTree
+    | Multiply of ArithmExprTree * ArithmExprTree   
 
 /// Посчитать значение дерева разбора арифметического выражения.
-let calculateValue arithmeticTree =
-    let matchParseOperation = 
-        System.NotImplementedException
-    
-    // ToDo : выполняет два раза каждый элемент
-    let rec loop operationOrValue operation neighbor= 
-        match operationOrValue with         
-        | Value(a) ->             
-            match operation with
-            | PlusOp ->
-                match neighbor with
-                | Value(b) -> a + b
-                | Plus(b, c) -> a + (loop b PlusOp c) + (loop b PlusOp c)
-                | Minus(b, c) -> a + (loop b MinusOp c) + (loop c MinusOp b)
-                | Division(b, c) -> a + (loop b DivisionOp c) + (loop c DivisionOp b)
-                | Multiply(b, c) -> a + (loop b MultiplyOp c) + (loop c MultiplyOp b)
-            | MinusOp ->
-                match neighbor with 
-                | Value(b) -> a - b
-                | Plus(b, c) -> a - (loop b PlusOp c) + (loop b PlusOp c)
-                | Minus(b, c) -> a - (loop b MinusOp c) + (loop c MinusOp b)
-                | Division(b, c) -> a - (loop b DivisionOp c) + (loop c DivisionOp b)
-                | Multiply(b, c) -> a - (loop b MultiplyOp c) + (loop c MultiplyOp b)
-            | DivisionOp ->
-                match neighbor with 
-                | Value(b) -> 
-                    if (b = 0) then 
-                        failwith "Произошло деление на ноль"
-                    else
-                        a / b
-                | Plus(b, c) -> a - (loop b PlusOp c) + (loop b PlusOp c)
-                | Minus(b, c) -> a - (loop b MinusOp c) + (loop c MinusOp b)
-                | Division(b, c) -> a - (loop b DivisionOp c) + (loop c DivisionOp b)
-                | Multiply(b, c) -> a - (loop b MultiplyOp c) + (loop c MultiplyOp b)
-            | MultiplyOp ->
-                match neighbor with 
-                | Value(b) -> a * b
-                | Plus(b, c) -> a * (loop b PlusOp c) + (loop b PlusOp c)
-                | Minus(b, c) -> a * (loop b MinusOp c) + (loop c MinusOp b)
-                | Division(b, c) -> a * (loop b DivisionOp c) + (loop c DivisionOp b)
-                | Multiply(b, c) -> a * (loop b MultiplyOp c) + (loop c MultiplyOp b)
-         //   \/ Нужен ли пробел?
-         | Plus(a, b) -> (loop a PlusOp b) + (loop b PlusOp a)         
-         | Minus(a, b) -> (loop a MinusOp b) - (loop b MinusOp a)
-         | Division(a, b) -> (loop a DivisionOp b) / (loop b DivisionOp a)
-         | Multiply(a, b) -> (loop a MultiplyOp b) * (loop b MultiplyOp a)         
-                
-    loop arithmeticTree NoneOp None
+let calculateValue arithmeticTree =       
+    let rec loop operationOrValue = 
+        match operationOrValue with   
+        //   \/  Нужен ли пробел?
+        | Plus(a, b) -> (loop a) + (loop b)         
+        | Minus(a, b) -> (loop a) - (loop b)
+        | Division(a, b) -> 
+            if (loop b = 0) then
+                failwith "Произошло деление на ноль."
+            else
+                (loop a) / (loop b)
+        | Multiply(a, b) -> (loop a) * (loop b)  
+        | Value(a) -> a                 
+                               
+    loop arithmeticTree
