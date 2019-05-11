11acf655be628165b3fa553d7cfff553e18f41d1
ArtyomAfanasov <artikul396@mail.ru>
2019-05-12 02:09:49 +0300

    наворотил дичи

--- IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
+++ IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
@@ -8,29 +8,66 @@ open PrimeInfinity
 type SimpleInfinityTestClass () =  
 
     [<Test>]
-    member this.``s`` () =
+    member this.``testtest`` () =
+        let sumSeq sequence1 = Seq.fold (fun acc elem -> acc + elem) 0 sequence1
+        Seq.init 10 (fun index -> index * index)
+        |> sumSeq
+        |> printfn "The sum of the elements is %d."        
+
+    [<Test>]
+    member this.``First 14 number should be prime.`` () =
         let primeInf = generatePrimeInfinity ()
+
+        printf "%A" primeInf
+
+        let rec loop step acc =
+            match step with            
+            | _ when (Seq.item step primeInf) = 2 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 3 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 5 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 7 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 11 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 13 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 17 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 19 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 23 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 29 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 31 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 37 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 41 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 43 -> loop (step + 1) (acc + 1)
+            | _ -> acc
+
+        loop 0 0 |> should equal 14
+
+    (*[<Test>]
+    member this.``First 14 number should be prime`` () =
+        let primeInf = generatePrimeInfinity ()
+        let mutable forCheckPrimeNumbers = 0
         (fun () -> 
             (Seq.fold (fun number acc -> 
                 match acc with
-                | 1 when number = 2 -> 0
-                | 2 when number = 3 -> 0
-                | 3 when number = 5 -> 0
-                | 4 when number = 7 -> 0
-                | 5 when number = 11 -> 0
-                | 6 when number = 13 -> 0
-                | 7 when number = 17 -> 0
-                | 8 when number = 19 -> 0
-                | 9 when number = 23 -> 0
-                | 10 when number = 29 -> 0
-                | 11 when number = 31 -> 0
-                | 12 when number = 37 -> 0
-                | 13 when number = 41 -> 0
-                | 14 when number = 43 -> 0
-                | 15 -> failwith "stop!") primeInf)
+                | 1 when number = 2 -> acc + 1
+                | 2 when number = 3 -> acc + 1
+                | 3 when number = 5 -> acc + 1
+                | 4 when number = 7 -> acc + 1
+                | 5 when number = 11 -> acc + 1
+                | 6 when number = 13 -> acc + 1
+                | 7 when number = 17 -> acc + 1
+                | 8 when number = 19 -> acc + 1
+                | 9 when number = 23 -> acc + 1
+                | 10 when number = 29 -> acc + 1
+                | 11 when number = 31 -> acc + 1
+                | 12 when number = 37 -> acc + 1
+                | 13 when number = 41 -> acc + 1
+                | 14 when number = 43 -> acc + 1
+                | 15 -> 
+                    forCheckPrimeNumbers <- acc
+                    failwith "stop!") 0 primeInf)
             |> ignore) |> should throw typeof<System.Exception>
 
-        let mutable forCheckPrimeNumbers = true
+        forCheckPrimeNumbers |> should equal 15      *)
+        (*let mutable forCheckPrimeNumbers = true
 
         (fun () -> 
             (Seq.fold (fun number acc ->
@@ -80,4 +117,6 @@ type SimpleInfinityTestClass () =
                 | 15 -> failwith "stop!") primeInf)  
             |> ignore) |> should throw typeof<System.Exception>
 
-        forCheckPrimeNumbers |> should equal true
+        forCheckPrimeNumbers |> should equal true*)
+
+    
--- IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
+++ IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
@@ -1,6 +1,28 @@
 ﻿/// Модуль с функцией, генерирующей бесконечную последовательность простых чисел.
 module PrimeInfinity
 
+/// Считает факториал от 'input'.
+let factorial input =   
+    match input with 
+    | _ when input > 0 ->
+        let rec loop input acc =
+            if input = 1 then
+                acc
+            else 
+                loop (input - 1) (acc * input)   
+        loop input 1
+
+    | 0 -> 1                            
+    | _ -> raise (System.ArgumentException("Факториал не определён для отрицательных чисел."))   
+
 /// Сгенерировать бесконечную последовательность простых чисел.
 let generatePrimeInfinity () =
-    System.NotImplementedException
+    Seq.initInfinite (fun index ->
+        let n = index + 1
+        if (n = 3) then
+            5
+        else            
+            let factorialOfN = factorial n
+            // Формула
+            let resForDebug = ((factorialOfN % (n + 1)) / n) * (n - 1) + 2
+            resForDebug)         
