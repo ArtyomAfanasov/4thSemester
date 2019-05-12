9edc8aa2d3cac41b50b1a6d6253e24e29c709bcb
ArtyomAfanasov <artikul396@mail.ru>
2019-05-12 03:44:21 +0300

    Готово.

--- IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
+++ IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
@@ -8,26 +8,30 @@ open PrimeInfinity
 type SimpleInfinityTestClass () =         
 
     [<Test>]
-    member this.``First 14 number should be prime.`` () =
+    member this.``Test for me. Show first 100 elements`` () =
         let primeInf = generatePrimeInfinity ()
 
         let rec loopPrint step =
             match step with 
-            | 14 -> 14
+            | 100 -> ()
             | _ -> 
-                printf "%d " <| Seq.item step primeInf
+                printfn "%i: число %d " (step + 1) (Seq.item step primeInf)
                 loopPrint (step + 1)
 
         loopPrint 0 |> ignore
+    
+    [<Test>]
+    member this.``First 14 number should be prime.`` () =
+        let primeInf = generatePrimeInfinity ()       
                
-
+        // Нумерация элементов в посл-ти начинается с нуля.
         let rec loop step acc =
             match step with            
             | _ when (Seq.item step primeInf) = 2 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 3 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 5 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 7 -> loop (step + 1) (acc + 1)
-            (*| _ when (Seq.item step primeInf) = 11 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 11 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 13 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 17 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 19 -> loop (step + 1) (acc + 1)
@@ -36,7 +40,44 @@ type SimpleInfinityTestClass () =
             | _ when (Seq.item step primeInf) = 31 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 37 -> loop (step + 1) (acc + 1)
             | _ when (Seq.item step primeInf) = 41 -> loop (step + 1) (acc + 1)
-            | _ when (Seq.item step primeInf) = 43 -> loop (step + 1) (acc + 1)*)
+            | _ when (Seq.item step primeInf) = 43 -> loop (step + 1) (acc + 1)
             | _ -> acc
 
         loop 0 0 |> should equal 14    
+
+    [<Test>]
+    member this.``27's, 167's, 657's and 1000's numbers should be prime.`` () =
+        let primeInf = generatePrimeInfinity ()       
+               
+        let rec loop step acc =
+            match step with            
+            | 26 when (Seq.item step primeInf) = 103 -> loop (step + 1) (acc + 1)
+            | 166 when (Seq.item step primeInf) = 991 -> loop (step + 1) (acc + 1)
+            | 656 when (Seq.item step primeInf) = 4919 -> loop (step + 1) (acc + 1)
+            | 999 when (Seq.item step primeInf) = 7919 -> loop (step + 1) (acc + 1)
+            | _ when step < 1001 -> loop (step + 1) acc
+            | _ -> acc
+
+        loop 0 0 |> should equal 4
+
+    [<Test>]
+    member this.``5000's numbers should be prime.`` () =
+        let primeInf = generatePrimeInfinity ()    
+
+        let rec loop step acc =
+            match step with            
+            | 4999 when (Seq.item step primeInf) = 48611 -> loop (step + 1) (acc + 1)            
+            | _ when step < 5000 -> loop (step + 1) acc
+            | _ -> acc
+
+        loop 0 0 |> should equal 1 
+        
+    [<Test>]
+    member this.``Crushtest on Euler's big prime number.`` () =
+        let primeInf = generatePrimeInfinity ()    
+        
+        let rec loop step =                                
+            if (Seq.item step primeInf) = 2147483647 then 1                                              
+            else loop (step + 1)                
+        
+        loop 0 |> should equal 1     
--- IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
+++ IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
@@ -1,57 +1,9 @@
 ﻿/// Модуль с функцией, генерирующей бесконечную последовательность простых чисел.
 module PrimeInfinity
 
-/// Считает факториал от 'input'.
-let factorial input =   
-    match input with 
-    | _ when input > 0 ->
-        let rec loop input acc =
-            if input = 1 then
-                acc
-            else 
-                loop (input - 1) (acc * input)   
-        loop input 1
-
-    | 0 -> 1                            
-    | _ -> raise (System.ArgumentException("Факториал не определён для отрицательных чисел."))   
-
 /// Сгенерировать бесконечную последовательность простых чисел.
-let generatePrimeInfinityOld () =
-    Seq.initInfinite (fun index ->
-        let n = index + 4
-        if (n = 3) then
-            5
-        else            
-            let factorialOfN = factorial n
-            // Формула
-            let resForDebug = ((factorialOfN % (n + 1)) / n) * (n - 1) + 2
-            resForDebug)      
-
-
-/// Сгенерировать бесконечную последовательность простых чисел.
-let generatePrimeInfinityNewOld () =
-    let dirtyInfinity = Seq.initInfinite (fun index ->
-        let number = index + 2
-                
-        let rec loop lessThanCurrentNumber =
-            match lessThanCurrentNumber with 
-            | number -> true
-            | _ -> 
-                if (number % lessThanCurrentNumber = 0) then false
-                else loop (lessThanCurrentNumber + 1)
-
-        let isPrime = loop 2
-        
-        if isPrime then number
-        else 0)
-
-    Seq.choose (fun x ->
-        match x with
-        | a when a <> 0 -> Some(a)
-        | _ -> None) dirtyInfinity
-
 let generatePrimeInfinity () =
-    Seq.initInfinite (fun index ->
+    let dirtyInfinity = Seq.initInfinite (fun index ->
         let number = index + 2
                 
         let rec loop lessThanCurrentNumber =
@@ -65,3 +17,8 @@ let generatePrimeInfinity () =
         
         if isPrime then number
         else 0)
+
+    Seq.choose (fun x ->
+        match x with
+        | a when a <> 0 -> Some(a)
+        | _ -> None) dirtyInfinity
