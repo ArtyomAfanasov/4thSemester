d92c5adefb1fa0d9f7c23f1672f01b4ff650c6b9
ArtyomAfanasov <artikul396@mail.ru>
2019-05-12 03:11:47 +0300

    Простые числа с перерывом из нулей.

--- IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
+++ IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
@@ -16,13 +16,52 @@ let factorial input =
     | _ -> raise (System.ArgumentException("Факториал не определён для отрицательных чисел."))   
 
 /// Сгенерировать бесконечную последовательность простых чисел.
-let generatePrimeInfinity () =
+let generatePrimeInfinityOld () =
     Seq.initInfinite (fun index ->
-        let n = index + 1
+        let n = index + 4
         if (n = 3) then
             5
         else            
             let factorialOfN = factorial n
             // Формула
             let resForDebug = ((factorialOfN % (n + 1)) / n) * (n - 1) + 2
-            resForDebug)         
+            resForDebug)      
+
+
+/// Сгенерировать бесконечную последовательность простых чисел.
+let generatePrimeInfinityNewOld () =
+    let dirtyInfinity = Seq.initInfinite (fun index ->
+        let number = index + 2
+                
+        let rec loop lessThanCurrentNumber =
+            match lessThanCurrentNumber with 
+            | number -> true
+            | _ -> 
+                if (number % lessThanCurrentNumber = 0) then false
+                else loop (lessThanCurrentNumber + 1)
+
+        let isPrime = loop 2
+        
+        if isPrime then number
+        else 0)
+
+    Seq.choose (fun x ->
+        match x with
+        | a when a <> 0 -> Some(a)
+        | _ -> None) dirtyInfinity
+
+let generatePrimeInfinity () =
+    Seq.initInfinite (fun index ->
+        let number = index + 2
+                
+        let rec loop lessThanCurrentNumber =
+            match lessThanCurrentNumber with 
+            | _ when number = lessThanCurrentNumber -> true
+            | _ -> 
+                if (number % lessThanCurrentNumber = 0) then false
+                else loop (lessThanCurrentNumber + 1)
+
+        let isPrime = loop 2
+        
+        if isPrime then number
+        else 0)
