7ee23dde4b500942484ac14da6a1cc3a335b6c43
ArtyomAfanasov <artikul396@mail.ru>
2019-05-18 00:35:53 +0300

    Init

---
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/BracketsSequence.Tests.fs
@@ -0,0 +1,12 @@
+namespace Hw5.Generics.Tests
+
+open BracketSequence
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type BracketSequenceTestClass () =
+
+    [<Test>]
+    member this.`` `` () =
+        Assert.IsTrue(true)
