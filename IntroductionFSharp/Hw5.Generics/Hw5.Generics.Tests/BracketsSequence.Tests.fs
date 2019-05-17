b6a227e55e6e4bc33734fffaa2f211ce59725652
ArtyomAfanasov <artikul396@mail.ru>
2019-05-18 01:24:39 +0300

    TDD

--- IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/BracketsSequence.Tests.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/BracketsSequence.Tests.fs
@@ -6,7 +6,71 @@ open FsUnit
 
 [<TestFixture>]
 type BracketSequenceTestClass () =
+    
+    [<Test>]
+    member this.``checkBrackets on data "((()))" should return true`` () =
+        checkBrackets "((()))" |> should equal true
 
     [<Test>]
-    member this.`` `` () =
-        Assert.IsTrue(true)
+    member this.``checkBrackets on data "[[[]]]" should return true`` () =
+        checkBrackets "[[[]]]" |> should equal true
+
+    [<Test>]
+    member this.``checkBrackets on data "{{{}}}" should return true`` () =
+        checkBrackets "{{{}}}" |> should equal true
+
+    [<Test>]
+    member this.``checkBrackets on data "))((" should return false`` () =
+        checkBrackets "))((" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "]][[" should return false`` () =
+        checkBrackets "]][[" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "}}{{" should return false`` () =
+        checkBrackets "}}{{" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "(]" should return false`` () =
+        checkBrackets "(]" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "[[[]]]" should return false`` () =
+        checkBrackets "[)" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "{{{}}}" should return false`` () =
+        checkBrackets "{)" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "" should return true`` () =
+        checkBrackets "" |> should equal true
+
+    [<Test>]
+    member this.``checkBrackets on data "(a[b{c}b]b)" should return true`` () =
+        checkBrackets "(a[b{c}b]b)" |> should equal true
+
+    [<Test>]
+    member this.``checkBrackets on data "(a[b{c}b)b]" should return false`` () =
+        checkBrackets "(a[b{c}b)b]" |> should equal false
+
+    [<Test>]
+    member this.``checkBrackets on data "(a[b{c{b]b)" should return false`` () =
+        checkBrackets "(a[b{c{b]b)" |> should equal false   
+
+    [<Test>]
+    member this.``checkBrackets on data ")a)b)c)b)b)" should return false`` () =
+        checkBrackets ")a)b)c)b)b)" |> should equal false   
+
+    [<Test>]
+    member this.``checkBrackets on data ") )" should return false`` () =
+        checkBrackets ") )" |> should equal false 
+        
+    [<Test>]
+    member this.``checkBrackets on data "( )" should return true`` () =
+        checkBrackets "( )" |> should equal true   
+
+    [<Test>]
+    member this.``checkBrackets on data "( asd" should return false`` () =
+        checkBrackets "( asd" |> should equal false   
