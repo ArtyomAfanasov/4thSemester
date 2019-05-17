1ad2ab92fd230750ae51c009f8850cea382d50d1
ArtyomAfanasov <artikul396@mail.ru>
2019-05-18 00:12:21 +0300

    Вместо Seq.choose использовал Seq.filter

--- IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
+++ IntroductionFSharp/Hw3/Hw3.Tests/PrimeInfinity.Tests.fs
@@ -81,3 +81,80 @@ type SimpleInfinityTestClass () =
             else loop (step + 1)                
         
         loop 0 |> should equal 1   
+
+
+    // Для Seq.filter.
+    [<Test>]
+    member this.``First 14 number should be prime. Filter`` () =
+        let primeInf = generatePrimeInfinityByFilter ()       
+               
+        // Нумерация элементов в посл-ти начинается с нуля.
+        let rec loop step acc =
+            match step with            
+            | _ when (Seq.item step primeInf) = 2 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 3 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 5 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 7 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 11 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 13 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 17 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 19 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 23 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 29 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 31 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 37 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 41 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step primeInf) = 43 -> loop (step + 1) (acc + 1)
+            | _ -> acc
+
+        loop 0 0 |> should equal 14    
+
+    [<Test>]
+    member this.``27's, 167's, 657's and 1000's numbers should be prime. ByFilter`` () =
+        let primeInf = generatePrimeInfinityByFilter ()       
+               
+        let rec loop step acc =
+            match step with            
+            | 26 when (Seq.item step primeInf) = 103 -> loop (step + 1) (acc + 1)
+            | 166 when (Seq.item step primeInf) = 991 -> loop (step + 1) (acc + 1)
+            | 656 when (Seq.item step primeInf) = 4919 -> loop (step + 1) (acc + 1)
+            | 999 when (Seq.item step primeInf) = 7919 -> loop (step + 1) (acc + 1)
+            | _ when step < 1001 -> loop (step + 1) acc
+            | _ -> acc
+
+        loop 0 0 |> should equal 4
+
+    [<Test>]
+    member this.``5000's numbers should be prime. ByFilter`` () =
+        let primeInf = generatePrimeInfinityByFilter ()    
+
+        let rec loop step acc =
+            match step with            
+            | 4999 when (Seq.item step primeInf) = 48611 -> loop (step + 1) (acc + 1)            
+            | _ when step < 5000 -> loop (step + 1) acc
+            | _ -> acc
+
+        loop 0 0 |> should equal 1             
+        
+    [<Test>]
+    member this.``10000's numbers should be prime. ByFilter`` () =
+        let primeInf = generatePrimeInfinityByFilter ()    
+
+        let rec loop step acc =
+            match step with            
+            | 9999 when (Seq.item step primeInf) = 104729 -> loop (step + 1) (acc + 1)            
+            | _ when step < 10001 -> loop (step + 1) acc
+            | _ -> acc
+
+        loop 0 0 |> should equal 1 
+
+    // Каждый раз считает элементы с начала до нужного => дооолго.
+    [<Test>]
+    member this.``14's number sould be prime ByFilter`` () =
+        let primeInf = generatePrimeInfinityByFilter ()    
+        
+        let rec loop step =                                
+            if ((Seq.item step primeInf) = 43) then 1                                              
+            else loop (step + 1)                
+        
+        loop 0 |> should equal 1   
--- IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
+++ IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
@@ -22,3 +22,22 @@ let generatePrimeInfinity () =
         match x with
         | a when a <> 0 -> Some(a)
         | _ -> None) dirtyInfinity
+
+/// Сгенерировать бесконечную последовательность простых чисел, используя Seq.filter
+let generatePrimeInfinityByFilter () =
+    let dirtyInfinity = Seq.initInfinite (fun index ->
+        let number = index + 2
+                
+        let rec loop lessThanCurrentNumber =
+            match lessThanCurrentNumber with 
+            | _ when number = lessThanCurrentNumber -> true
+            | _ -> 
+                if (number % lessThanCurrentNumber = 0) then false
+                else loop (lessThanCurrentNumber + 1)
+
+        let isPrime = loop 2
+        
+        if isPrime then number
+        else 0)
+
+    Seq.filter (fun x -> x <> 0) dirtyInfinity
