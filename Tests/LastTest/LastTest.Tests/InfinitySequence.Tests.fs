496fd02df10ff0a3fd39713c4849f9d7a39b3f46
ArtyomAfanasov <artikul396@mail.ru>
2019-05-28 16:55:45 +0300

    Странно работает

---
+++ Tests/LastTest/LastTest.Tests/InfinitySequence.Tests.fs
@@ -0,0 +1,44 @@
+namespace LastTest.Tests
+
+open NUnit.Framework
+open FsUnit
+open InfinitySequence
+
+[<TestFixture>]
+type InfinitySequenceTestClass () =
+
+    [<Test>]
+    member this.``First 14 numbers should be correct.`` () =
+        let seq = generateInfinitySequence ()       
+                
+        let rec loop step acc =
+            match step with             
+            | _ when (Seq.item step seq) = 1 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 2 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 2 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 3 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 3 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 3 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 4 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 4 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 4 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 4 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 5 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 5 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 5 -> loop (step + 1) (acc + 1)
+            | _ when (Seq.item step seq) = 5 -> loop (step + 1) (acc + 1)
+            | _ -> acc
+
+        loop 0 0 |> should equal 14   
+        
+    [<Test>]
+    member this.``3's numbers in seq should be 2.`` () =
+        let seq = generateInfinitySequence ()           
+
+        Seq.item 3 seq |> should equal 2
+
+    [<Test>]
+       member this.``1's numbers in seq should be 1.`` () =
+           let seq = generateInfinitySequence ()           
+
+           Seq.item 0 seq |> should equal 1
---
+++ Tests/LastTest/LastTest/InfinitySequence.fs
@@ -0,0 +1,20 @@
+﻿/// Модуль с функцией описывающей бесконечную последовательность 
+/// [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, …].
+module InfinitySequence
+
+/// Сгенерировать последовательность [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, …].
+let generateInfinitySequence () =   
+    (1, 1)
+    |>
+    Seq.unfold (fun (state, curIndex) ->                
+        match curIndex with 
+        | _ when curIndex < (state * ((1 + state)/2)) -> 
+            Some(state, (state, (curIndex + 1)))
+        | _ when curIndex = (state * ((1 + state)/2)) -> 
+            Some(state, (state + 1, (curIndex + 1)))
+        )
+
+[<EntryPoint>]
+let main argv = 
+    Seq.iter (fun elem -> printfn "%d " elem) (generateInfinitySequence ())
+    0
