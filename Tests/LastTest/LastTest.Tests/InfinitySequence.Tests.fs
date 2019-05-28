6296667307e31223f6b3eac16e621649ce7bf65e
ArtyomAfanasov <artikul396@mail.ru>
2019-05-28 17:03:20 +0300

    тесты

--- Tests/LastTest/LastTest.Tests/InfinitySequence.Tests.fs
+++ Tests/LastTest/LastTest.Tests/InfinitySequence.Tests.fs
@@ -35,7 +35,13 @@ type InfinitySequenceTestClass () =
     member this.``3's numbers in seq should be 2.`` () =
         let seq = generateInfinitySequence ()           
 
-        Seq.item 3 seq |> should equal 2
+        Seq.item 2 seq |> should equal 2
+
+    [<Test>]
+    member this.``325's numbers in seq should be 2.`` () =
+        let seq = generateInfinitySequence ()           
+
+        Seq.item 324 seq |> should equal 25
 
     [<Test>]
        member this.``1's numbers in seq should be 1.`` () =
