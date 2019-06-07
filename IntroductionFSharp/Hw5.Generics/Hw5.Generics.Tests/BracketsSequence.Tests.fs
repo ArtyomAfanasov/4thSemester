92fbbc6fcc4992dc51f4a2ce6bb2e7a580c16fd0
ArtyomAfanasov <artikul396@mail.ru>
2019-06-07 16:07:28 +0300

    работает не для всех случаев

--- IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/BracketsSequence.Tests.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/BracketsSequence.Tests.fs
@@ -9,71 +9,85 @@ type BracketSequenceTestClass () =
     
     [<Test>]
     member this.``checkBrackets on data "((()))" should return true`` () =
-        checkBrackets ("((()))".ToCharArray()) (First('(', ')')) (Second(null, null)) (Third(null, null)) 
+        checkBrackets "((()))" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000') 
         |> should equal true
-
-    (*
+    
     [<Test>]
     member this.``checkBrackets on data "[[[]]]" should return true`` () =
-        checkBrackets "[[[]]]" |> should equal true
+        checkBrackets "[[[]]]" ('[', ']') ('\u0000', '\u0000') ('\u0000', '\u0000') 
+        |> should equal true
 
     [<Test>]
     member this.``checkBrackets on data "{{{}}}" should return true`` () =
-        checkBrackets "{{{}}}" |> should equal true
+        checkBrackets "{{{}}}" ('{', '}') ('\u0000', '\u0000') ('\u0000', '\u0000') 
+        |> should equal true
 
     [<Test>]
     member this.``checkBrackets on data "))((" should return false`` () =
-        checkBrackets "))((" |> should equal false
+        checkBrackets "))((" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
     member this.``checkBrackets on data "]][[" should return false`` () =
-        checkBrackets "]][[" |> should equal false
+        checkBrackets "]][[" ('[', ']') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
     member this.``checkBrackets on data "}}{{" should return false`` () =
-        checkBrackets "}}{{" |> should equal false
+        checkBrackets "}}{{" ('{', '}') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
     member this.``checkBrackets on data "(]" should return false`` () =
-        checkBrackets "(]" |> should equal false
+        checkBrackets "(]" ('(', ')') ('[', ']') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
-    member this.``checkBrackets on data "[[[]]]" should return false`` () =
-        checkBrackets "[)" |> should equal false
+    member this.``checkBrackets on data "[)" should return false`` () =
+        checkBrackets "[)" ('(', ')') ('[', ']') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
-    member this.``checkBrackets on data "{{{}}}" should return false`` () =
-        checkBrackets "{)" |> should equal false
+    member this.``checkBrackets on data "{)" should return false`` () =
+        checkBrackets "{)" ('(', ')') ('{', '}') ('\u0000', '\u0000')
+        |> should equal false
 
     [<Test>]
     member this.``checkBrackets on data "" should return true`` () =
-        checkBrackets "" |> should equal true
+        checkBrackets "" ('(', ')') ('[', ']') ('{', '}')
+        |> should equal true
 
     [<Test>]
     member this.``checkBrackets on data "(a[b{c}b]b)" should return true`` () =
-        checkBrackets "(a[b{c}b]b)" |> should equal true
+        checkBrackets "(a[b{c}b]b)" ('(', ')') ('{', '}') ('[', ']')
+        |> should equal true
 
     [<Test>]
     member this.``checkBrackets on data "(a[b{c}b)b]" should return false`` () =
-        checkBrackets "(a[b{c}b)b]" |> should equal false
+        checkBrackets "(a[b{c}b)b]" ('(', ')') ('{', '}') ('[', ']')
+        |> should equal false
 
     [<Test>]
     member this.``checkBrackets on data "(a[b{c{b]b)" should return false`` () =
-        checkBrackets "(a[b{c{b]b)" |> should equal false   
+        checkBrackets "(a[b{c{b]b)" ('(', ')') ('{', '}') ('[', ']')
+        |> should equal false   
 
     [<Test>]
     member this.``checkBrackets on data ")a)b)c)b)b)" should return false`` () =
-        checkBrackets ")a)b)c)b)b)" |> should equal false   
+        checkBrackets ")a)b)c)b)b)" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false   
 
     [<Test>]
     member this.``checkBrackets on data ") )" should return false`` () =
-        checkBrackets ") )" |> should equal false 
+        checkBrackets ") )" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false 
         
     [<Test>]
     member this.``checkBrackets on data "( )" should return true`` () =
-        checkBrackets "( )" |> should equal true   
+        checkBrackets "( )" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal true   
 
     [<Test>]
     member this.``checkBrackets on data "( asd" should return false`` () =
-        checkBrackets "( asd" |> should equal false   
-    *)
+        checkBrackets "( asd" ('(', ')') ('\u0000', '\u0000') ('\u0000', '\u0000')
+        |> should equal false       
--- IntroductionFSharp/Hw5.Generics/Hw5.Generics/BracketsSequence.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics/BracketsSequence.fs
@@ -3,31 +3,108 @@
 /// Скобки бывают трёх видов.
 module BracketSequence 
 
-open System.Text.RegularExpressions
-
-/// Собственный тип. 'a, 'b, 'c - конкретные значения типов.
+(*
+/// Собственный тип. 'a - конкретное значение типа.
 ///
 /// Remarks: подходит и для скобок.
-type ObjectType<'a, 'b, 'c> =
-    | First of 'a * 'a
-    | Second of 'b * 'b 
-    | Third of 'c * 'c
+type ObjectType<'a> =
+    | FirstValue of 'a * 'a
+    | SecondValue of 'a * 'a
+    | ThirdValue of 'a * 'a
+*)
 
+/// Проверить корректность последовательности выбранных символов.
+/// Первый элемент пары - это открывающий символ. Второй - закрывающий.
+///
+/// Если какая-то из скобок не нужна, то задайте для неё пару значений char по умолчанию ('\u0000', '\u0000').
+/// ? Можно было бы сделать три функции, чтобы пользователь не заморачивался. Как лучше ?
+let checkBrackets (input : string) (firstValue : char* char) (secondValue : char* char) (thirdValue : char* char) =
+    let openF, closeF = firstValue
+    let openS, closeS = secondValue
+    let openT, closeT = thirdValue
+    
+    // Из (()) получу ))((.
+    let listsOfOpenAndCloseValues =
+        Seq.fold (fun (firstList, secondList, thirdList)(*((listOpenF, listCloseF), (listOpenS, listCloseS), (listOpenT, listCloseT))*) elem ->
+            match elem with 
+            | def when def = '\u0000' -> (firstList, secondList, thirdList)
+            | a when a = openF -> (elem :: firstList, secondList, thirdList)
+            | b when b = closeF -> (elem :: firstList, secondList, thirdList)
+            | c when c = openS -> (firstList, elem :: secondList, thirdList)
+            | d when d = closeS -> (firstList, elem :: secondList, thirdList)
+            | e when e = openT -> (firstList, secondList, elem :: thirdList)
+            | f when f = closeT -> (firstList, secondList, elem :: thirdList)
+            | _ -> (firstList, secondList, thirdList)
+           ) ([], [], []) input
+    
+    let firstCouples, secondCouples, thirdCouples = listsOfOpenAndCloseValues
+        
+    let rec loop list number =
+        let checkList list openBr closeBr = 
+            match list with   
+            | [] -> true
+            | head :: tail -> 
+                if head = closeBr && List.last tail = openBr then 
+                    loop (List.rev (List.rev tail).Tail) number
+                else false
+
+        match number with 
+        | 1 -> checkList list openF closeF                        
+        | 2 -> checkList list openS closeS 
+        | 3 -> checkList list openT closeT 
+
+    loop firstCouples 1 && loop secondCouples 2 && loop thirdCouples 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Seq.choose (fun elem ->
+    //    match elem with 
+    //    | def when def = '\u0000' -> None
+    //    | a when a = openF -> Some(elem)
+    //    | b when b = closeF -> Some(elem)
+    //    | c when c = openS -> Some(elem)
+    //    | d when d = closeS -> Some(elem)
+    //    | e when e = openT -> Some(elem)
+    //    | f when f = closeT -> Some(elem)
+    //    | _ -> None
+    //
+    //    ) inputWithSeq            
+
+(*
 /// Проверить корректность последовательности выбранных типов.
 /// first/second/thirdType - рассматриваемые типы.
-/// Если какой-то из типов не нужен, то задайте для него конкретную пару значений (null, null).
+/// Если какой-то из типов не нужен, то задайте для него конкретную пару значений по умолчанию
 ///
 /// Remarks: подходит и для скобочной последовательности.
 let checkBrackets inputWithSeq firstType secondType thirdType =        
     match firstType, secondType, thirdType with
-    | First (a, b), Second (c, d), Third (e, f) -> 
+    | FirstValue (a, b), SecondValue (c, d), ThirdValue (e, f) -> 
         let openF = a
         let closeF= b
         let openS = c
         let closeS = d
         let openT = e
         let closeT = f
-
+        
         let countOfOpenAndCloseValues = 
             (Seq.fold (fun ((oF, cF), (oS, cS), (oT, cT)) elem ->
                 match elem with 
@@ -47,10 +124,12 @@ let checkBrackets inputWithSeq firstType secondType thirdType =
         if (countOpenFirst = countCloseFirst && 
             countOpenSecond = countCloseSecond &&
             countOpenThird = countCloseThird) then true
-        else false
+        else false       
                
     | _ -> failwith "Should never get here."    
+*)
+
 
 [<EntryPoint>]
-let main argv =      
+let main argv =         
     0
