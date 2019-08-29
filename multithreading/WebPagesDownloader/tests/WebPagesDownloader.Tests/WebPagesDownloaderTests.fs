96a1918135fd28eae12a81d866a48ffa092db1f5
Артём Афанасов <Afanasov.AK@esphere.ru>
2019-08-29 19:50:37 +0300

    Каркас готов. Не работает :)

---
+++ multithreading/WebPagesDownloader/tests/WebPagesDownloader.Tests/WebPagesDownloaderTests.fs
@@ -0,0 +1,16 @@
+﻿namespace WebPagesDownloader.Tests
+
+open NUnit.Framework
+open FsUnit
+open WebPagesDownloader
+
+[<TestFixture>]
+type WebPagesDownloaderTestsClass () =
+    [<Test>]
+    member this.``t`` () =
+        // arrange
+        
+        // act
+        
+        // assert
+        ()
