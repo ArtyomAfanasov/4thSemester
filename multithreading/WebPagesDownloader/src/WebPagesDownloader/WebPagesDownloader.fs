93b2d9555e74fbc821c0d3588f3550f30be45c73
Артём Афанасов <Afanasov.AK@esphere.ru>
2019-08-30 17:51:27 +0300

    Корректо находит информацию о внутренних страницах. Нужно обработать исключения.

--- multithreading/WebPagesDownloader/src/WebPagesDownloader/WebPagesDownloader.fs
+++ multithreading/WebPagesDownloader/src/WebPagesDownloader/WebPagesDownloader.fs
@@ -8,146 +8,83 @@ module WebPagesDownloader
     open System.IO
     open System.Collections.Generic
 
-    /// Регулярное выражение для поиска ссылок на указанной странице.
-    let findInnerWebPages = new Regex(@"<a href=""http://(\w*)"">")
-
-    ///// Скачать html.
-    //let downloadHtml url = 
-    //    async {
-    //        do printfn "Creating request for %s..." url
-    //        let request = WebRequest.Create(url)
-    //        use! response = request.AsyncGetResponse()
-    //        do printfn "Getting response stream for %s..." url
-    //        use stream = response.GetResponseStream()
-    //        do printfn "Reading response for %s..." url
-    //        use reader = new StreamReader(stream)
-    //        return reader.ReadToEnd()
-    //    }
-    //
-    ///// Вывести все пары `(адрес страницы * число символов)` для веб-страниц, указанных в данной.
-    //let printSizeOfAllInnerWebPages url =
-    //    async {
-    //        let! mainHtml = downloadHtml url
-    //        
-    //        let innerPages = new List<string>()
-    //
-    //        let matches = findInnerWebPages.Matches(mainHtml)
-    //        for match' in matches do
-    //            if match'.Success then 
-    //                let captures = match'.Captures
-    //                for capture in captures do   
-    //                    innerPages.Add capture.Value
-    //            else printfn "Внутренних страниц нет."    
-    //
-    //        //let countOfInnerPages = findInnerWebPages.Matches(mainHtml).Count
-    //        //let mutable innerPages = Array.zeroCreate<string> countOfInnerPages
-    //        //findInnerWebPages.Matches(mainHtml).CopyTo(innerPages, 0)
-    //
-    //        do printfn "Print couple `ulr * amount of characters` for inner web pages..."
-    //        
-    //        innerPages
-    //        |> Seq.map (fun url -> 
-    //            async {
-    //                let! html = downloadHtml url
-    //                (printfn "%s --- %s" url, html.Length) 
-    //                |> ignore
-    //            })
-    //        |> Async.Parallel
-    //        |> Async.RunSynchronously
-    //        |> ignore
-    //
-    //        printfn "It's end."
-    //    }
-    //    |> ignore                       
+    /// Загрузчик информации о внутренних веб-страницах указанной страницы.
+    type WebPagesDownloader() = 
+        /// Регулярное выражение для поиска ссылок на указанной странице.
+        let findInnerWebPages = new Regex(@"a href=""http://(\S*)""")
+        
+        /// Нормализовать найденный шаблон `a href="http://..."` до `http://...`
+        let normalizeUrl = new Regex(@"a href=|""")
     
-    /// Вывести все пары `(адрес страницы * число символов)` для веб-страниц, указанных в данной.
-    let printSizeOfAllInnerWebPages url =
-        async {
-            do printfn "Creating request for %s..." url
-            let request = WebRequest.Create(url)
-            use! response = request.AsyncGetResponse()
-            do printfn "Getting response stream for %s..." url
-            use stream = response.GetResponseStream()
-            do printfn "Reading response for %s..." url
-            use reader = new StreamReader(stream)
-            let mainHtml = reader.ReadToEnd()            
-            
-            let innerPages = new List<string>()
-
-            let matches = findInnerWebPages.Matches(mainHtml)
-            for match' in matches do
-                if match'.Success then 
-                    let captures = match'.Captures
-                    for capture in captures do   
-                        innerPages.Add capture.Value
-                else printfn "Внутренних страниц нет."    
-
-            //let countOfInnerPages = findInnerWebPages.Matches(mainHtml).Count
-            //let mutable innerPages = Array.zeroCreate<string> countOfInnerPages
-            //findInnerWebPages.Matches(mainHtml).CopyTo(innerPages, 0)
-
-            do printfn "Print couple `ulr * amount of characters` for inner web pages..."
-            
-            innerPages
-            |> Seq.map (fun url -> 
-                async {
-                    do printfn "Creating request for %s..." url
-                    let request = WebRequest.Create(url)
-                    use! response = request.AsyncGetResponse()
+        /// Скачать html.
+        let downloadHtml url = 
+            async {
+                do printfn "Creating request for %s..." url
+                let request = WebRequest.Create(url)
+                try
+                    use! response = request.AsyncGetResponse() 
                     do printfn "Getting response stream for %s..." url
                     use stream = response.GetResponseStream()
                     do printfn "Reading response for %s..." url
                     use reader = new StreamReader(stream)
-                    let html = reader.ReadToEnd()   
-                    (printfn "%s --- %s" url, html.Length) 
-                    |> ignore
+                    return reader.ReadToEnd()
+                with 
+                | :? System.Net.WebException -> 
+                    return url + " not found."
+                | :? System.Exception as e -> 
+                    do printfn "%s" e.Message
+                    return e.Message
+            }
+
+        /// Применить регулярное выражение к html-коду страницы.
+        let matchHtml html (pagesList : List<string>) = 
+            let matches = findInnerWebPages.Matches(html)
+            for match' in matches do
+                if match'.Success then 
+                    let captures = match'.Captures
+                    for capture in captures do   
+                        pagesList.Add (normalizeUrl.Replace(capture.Value, System.String.Empty))
+                else printfn "No internal pages here."    
+            pagesList
+
+        /// Вывести данные о внутренних страницах.
+        let printSize pagesList = 
+            pagesList
+            |> Seq.map (fun url -> 
+                async {
+                    let! html = downloadHtml url
+                    match html with
+                    | x when x = url + " not found" -> printfn "%s not found" url
+                    | _ -> 
+                        do printfn "%s --- %i" url html.Length
                 })
-            |> Async.Parallel
+            |> Async.Parallel            
             |> Async.RunSynchronously
             |> ignore
 
-            printfn "It's end."
-        }
-        |> ignore        
+        /// Вывести все пары `(адрес страницы * число символов)` для веб-страниц, указанных в данной.
+        let printSizeOfAllInnerWebPages url =
+            async {
+                let! mainHtml = downloadHtml url
+                match mainHtml with
+                | x when x = url + " not found" -> printfn "%s not found" url
+                | _ ->            
+                    let innerPages = matchHtml mainHtml (new List<string>())
+    
+                    do printfn "Print couple `ulr * amount of characters` for inner web pages..."
+            
+                    do printSize innerPages
+    
+                    do printfn "That's all."
+            }
+
+        /// Вывести все пары `(адрес страницы * число символов)` для веб-страниц, указанных в данной.
+        member this.PrintSizeOfAllInnerWebPages url = printSizeOfAllInnerWebPages url
 
     [<EntryPoint>]
-    let main arg = 
-        //let regex = new Regex(@"<a href=""http://(\w*)"">")
-        //let matches = regex.Matches(@"adasdawe21e21easd <a href=""http://ad12eas2""> asd21e211da <a href=""http://ad1213asd2eas2"">")
-        //for match' in matches do
-        //    if match'.Success then 
-        //        //let groups = match'.Groups
-        //        let captures = match'.Captures
-        //        let countOfInnerPages = captures.Count
-        //        let mutable innerPages = Array.zeroCreate<string> countOfInnerPages
-        //        for capture in captures do   
-        //            printfn "%s" capture.Value
-        //    else printfn "sorry"    
-        
-        // let matches = findInnerWebPages.Matches(@"adasdawe21e21easd <a href=""http://se.math.spbu.ru""> asd21e211da <a href=""http://spisok.math.spbu.ru"">")
-        //
-        ///////////////let innerPages = new List<string>()
-        ///////////////let matches = findInnerWebPages.Matches(@"adasdawe21e21easd <a href=""htasd213""> asd21e211da <a href=""http://se.math.spbu.ru"">")
-        ///////////////for match' in matches do
-        ///////////////    if match'.Success then 
-        ///////////////        let captures = match'.Captures
-        ///////////////        for capture in captures do   
-        ///////////////            innerPages.Add capture.Value
-        ///////////////    else printfn "Внутренних страниц нет."    
-        ///////////////
-        ///////////////innerPages
-        ///////////////|> Seq.map (fun url -> 
-        ///////////////    async {
-        ///////////////        let! html = downloadHtml url
-        ///////////////        (printfn "%s --- %s" url, html.Length) 
-        ///////////////        |> ignore
-        ///////////////    })
-        ///////////////|> Async.Parallel
-        ///////////////|> Async.RunSynchronously
-        ///////////////|> ignore
-        //printSizeOfAllInnerWebPages @"http://se.math.spbu.ru"       
-        //System.Console.ReadLine() |> ignore
-
-        printSizeOfAllInnerWebPages @"http://se.math.spbu.ru"
-
+    let main arg =  
+        let cs = WebPagesDownloader()
+        cs.PrintSizeOfAllInnerWebPages @"http://se.math.spbu.ru/SE"
+        |> Async.RunSynchronously
+        |> ignore
         0
