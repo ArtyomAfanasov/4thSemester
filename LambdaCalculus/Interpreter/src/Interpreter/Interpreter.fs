b33d1d2945e64dd129a7f317c14a2e034f66ca21
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-09-11 10:41:14 +0300

    Черновик

--- LambdaCalculus/Interpreter/src/Interpreter/Interpreter.fs
+++ LambdaCalculus/Interpreter/src/Interpreter/Interpreter.fs
@@ -1,8 +1,13 @@
 ﻿module Interpreter
 
-    type LambdaTree =   
-        | Term
-        | Lambda of LambdaTree
+    // ((λa.(λb.b b) (λb.b b)) b) ((λc.(c b)) (λa.a))
+    type LambdaTree<'a> =           
+        | LeftBracket
+        | RightBracket
+        | End
+        | LambdaArgument of LambdaTree<'a>
+        | AbstractArgument of LambdaTree<'a>
+        | Lambda of LambdaTree<'a>
 
     let normalize lambda =
         let rec loop =
