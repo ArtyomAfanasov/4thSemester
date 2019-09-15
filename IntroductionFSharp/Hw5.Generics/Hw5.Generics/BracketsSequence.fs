21cb373957b32f28e8826d752d6190a9d62d7184
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-09-15 15:20:13 +0300

    Рефакторинг BracketSequence. Нет мутабельных структур данных.

--- IntroductionFSharp/Hw5.Generics/Hw5.Generics/BracketsSequence.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics/BracketsSequence.fs
@@ -12,16 +12,20 @@ let closedBrackets = Map.ofList [ (')', '('); ('}', '{'); (']', '['); ('>', '<')
 let openedBrackets = ['('; '['; '{'; '<']    
 
 /// Проверить корректность скобочной последовательности.
-let checkBrackets (input : string) = 
-    let stack = Stack<char>()
-    
-    String.iter (fun elem -> 
+let checkBrackets (input : string) =     
+    List.fold (fun stack elem -> 
         if closedBrackets.ContainsKey elem || List.contains elem openedBrackets then        
-            if stack.Count = 0 then stack.Push elem            
-            elif List.contains elem openedBrackets then stack.Push(elem)            
-            elif closedBrackets.Item elem = stack.Peek() then
-                stack.Pop() |> ignore
-            else stack.Push elem
-        else ()) input
+            if List.length stack = 0 then (elem :: stack)
+            elif List.contains elem openedBrackets then (elem :: stack)           
+            elif (match stack with
+                    | h :: t -> h = closedBrackets.Item elem
+                    | _ -> failwith "Should never get here") then
+                        match stack with
+                        | h :: t -> t
+                        | _ -> failwith "Should never get here"
+            else (elem :: stack)
+        else stack) [] (Seq.toList input)
+    |> List.length 
+    |> (=) 0
 
-    stack.Count = 0
+    // F# красив :)
