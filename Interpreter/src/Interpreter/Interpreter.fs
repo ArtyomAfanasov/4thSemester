cd693a07349edc408358acbdc75f18e9d5d33707
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-10-08 22:52:32 +0300

    Рефакторинг тестов и интерпретатора.

--- Interpreter/src/Interpreter/Interpreter.fs
+++ Interpreter/src/Interpreter/Interpreter.fs
@@ -3,49 +3,60 @@ module Interpreter
  
 type Term = 
     | Variable of char
-    | Applique of Term * Term
-    | LambdaAbstract of char * Term
+    | Application of Term * Term
+    | LambdaAbstraction of char * Term
 
 /// Выполнить альфа-конверсию.
 let prepareToAlphaConversion outerTerm =
     let rec setAlphabet alphabet term =
         match term with
         | Variable(_) -> alphabet
-        | Applique(func, argument) -> 
+        | Application(func, argument) -> 
             (setAlphabet alphabet func) @ (setAlphabet alphabet argument)
-        | LambdaAbstract(name, nextTerm) -> setAlphabet (name :: alphabet) nextTerm
+        | LambdaAbstraction(name, nextTerm) -> setAlphabet (name :: alphabet) nextTerm
 
     setAlphabet [] outerTerm
 
-/// Выполнить бетта-редукцию.
-let bettaReduction replacementName termForSubstitution argument =
-    let rec reduce term =
+/// Выполнить подстановку.
+let performSubstitution replacementName termForSubstitution argument =
+    let rec perform term =
         match term with
         | Variable(nameInLamdaAbstract) ->
             if nameInLamdaAbstract = replacementName then argument
-            else Variable(nameInLamdaAbstract)
-        | LambdaAbstract(name, nextTerm) -> LambdaAbstract(name, reduce nextTerm)
-        | Applique(func, innerArgument) ->
-            match func with 
-            | Variable(_) -> Applique(reduce func, reduce innerArgument)
-            | Applique(_, _) -> 
-                let debug = Applique(reduce func, reduce innerArgument)
-                debug
-            | LambdaAbstract(name, nextTerm) -> 
-                let debug = Applique(LambdaAbstract(name, reduce nextTerm), innerArgument)
-                debug
+            else term
+        | LambdaAbstraction(name, nextTerm) -> LambdaAbstraction(name, perform nextTerm)
+        | Application(func, innerArgument) -> Application(perform func, perform innerArgument)
 
-    reduce termForSubstitution
+    perform termForSubstitution
 
+            //match func with 
+            //| Variable(_) -> Application(perform func, perform innerArgument)
+            //| Application(_, _) ->
+            //    let debug = Application(perform func, perform innerArgument)
+            //    debug
+            //| LambdaAbstraction(name, nextTerm) -> 
+            //    let debug = Application(LambdaAbstraction(name, perform nextTerm), innerArgument)
+            //    debug
+    
+// TODO Нормальный подсчёт всех аппликаций, либо другой вариант цикла.
 /// Сосчитать количество аппликаций.
 let countApplique outerTerm =
-    let rec count number term =
+    let rec countLeft listForCounting term =
         match term with
-        | Applique(func, _) -> count (number + 1) func
-        | Variable(_) -> number
-        | LambdaAbstract(_, nextTerm) -> count number nextTerm
+        | Application(func, _) -> 
+            countLeft (listForCounting + 1) func
+        | Variable(_) -> listForCounting
+        | LambdaAbstraction(_, nextTerm) -> countLeft listForCounting nextTerm
 
-    count 0 outerTerm
+    let rec countRight listForCounting term =
+        match term with
+        | Application(_, argument) -> 
+            countRight (listForCounting + 1) argument
+        | Variable(_) -> listForCounting
+        | LambdaAbstraction(_, nextTerm) -> countRight listForCounting nextTerm
+
+    let numberOfApplications = countLeft 0 outerTerm
+    countRight numberOfApplications outerTerm
 
 /// Нормализовать терм.
 let normalizeTerm outerTerm =
@@ -53,13 +64,13 @@ let normalizeTerm outerTerm =
 
     let rec findAndReduceRedex term =
         match term with
-        | Variable(name) -> Variable(name)
-        | LambdaAbstract(name, nextTerm) -> LambdaAbstract(name, findAndReduceRedex nextTerm)
-        | Applique(func, argument) -> 
+        | Variable(name) -> term
+        | LambdaAbstraction(name, nextTerm) -> LambdaAbstraction(name, findAndReduceRedex nextTerm)
+        | Application(func, argument) -> 
             match func with
-            | Variable(name) -> Applique(Variable(name), argument)
-            | Applique(_, _) -> Applique(findAndReduceRedex func, argument)
-            | LambdaAbstract(name, nextTerm) -> bettaReduction name nextTerm argument
+            | Variable(_) -> Application(func, findAndReduceRedex argument)
+            | Application(_, _) -> Application(findAndReduceRedex func, findAndReduceRedex argument)
+            | LambdaAbstraction(name, nextTerm) -> performSubstitution name nextTerm argument
 
     let rec loop step term =
         if step > (appliqueNumber) then term
@@ -67,9 +78,4 @@ let normalizeTerm outerTerm =
             let newTerm = findAndReduceRedex term
             loop (step + 1) newTerm
 
-    loop 0 outerTerm
-        
-
-[<EntryPoint>]
-let main argv =
-    0
+    loop 0 outerTerm        
--- Interpreter/tests/Interpreter.Tests/Interpreter.Tests.fs
+++ Interpreter/tests/Interpreter.Tests/Interpreter.Tests.fs
@@ -8,126 +8,167 @@ open Interpreter
 type InterpreterTestClass () =
     [<Test>]
     member this.``Test should correct normalize `(\l x. x) b` with 'b' result.`` () =
-        (Applique(LambdaAbstract('x', Variable('x')), Variable('b')))
+        (Application(LambdaAbstraction('x', Variable('x')), Variable('b')))
         |>
         normalizeTerm |> should equal (Variable('b'))
     
+    [<Test>]    
+    member this.``Test should correct with right redex `x ((\l a. a) b)` with 'b' result.`` () =
+        (Application(Variable('x'), Application(LambdaAbstraction('a', Variable('a')), Variable ('b'))))
+        |>
+        normalizeTerm |> should equal (Application(Variable('x'), Variable('b')))
+    
     [<Test>]
     member this.``Test should correct normalize `(\l x. \l y. x y) b` with ... result.`` () =
-        (Applique(LambdaAbstract('x', LambdaAbstract('y', Applique(Variable('x'), Variable('y')))), Variable('b')))
+        (Application(LambdaAbstraction('x', LambdaAbstraction('y', Application(Variable('x'), Variable('y')))), Variable('b')))
         |>
-        normalizeTerm |> should equal (LambdaAbstract('y', Applique(Variable('b'), Variable('y'))))
+        normalizeTerm |> should equal (LambdaAbstraction('y', Application(Variable('b'), Variable('y'))))
     
     // Can i avoid such terrible DU?
     [<Test>]
-    member this.``Test should correct count applique `((\l x. \l y. x y x) b) c` with ... result.`` () =
-        Applique(
-            Applique(
-                LambdaAbstract('x', 
-                    LambdaAbstract('y', 
-                        Applique(
-                            Applique(
+    member this.``Test should correct count left applications `((\l x. \l y. x y x) b) c` with ... result.`` () =
+        Application(
+            Application(
+                LambdaAbstraction('x', 
+                    LambdaAbstraction('y', 
+                        Application(
+                            Application(
                                 Variable('x'), Variable('y')), Variable('x')))), Variable('b')), Variable('c'))
-        |> countApplique |> should equal 4
+        |> countApplique |> should equal 4    
+    
+    [<Test>]
+    member this.``Test should correct count left and right applications `((\l x. \l y. \l z. x z (y z)) (\l x. \l y. x)) \l x. \l y. x` with ... result.`` () =
+        let firstPart = 
+            LambdaAbstraction('x', 
+                LambdaAbstraction('y', 
+                    LambdaAbstraction('z', 
+                        Application(
+                            Application(Variable('x'), Variable('z')), Application(Variable('y'), Variable('z'))))))    
+                
+        let secondPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
+
+        let thirdPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
+
+        Application(Application(firstPart, secondPart), thirdPart)
+        |> countApplique |> should equal 5    
+    
+    [<Test>]
+    member this.``Test should correct normalize with right redex`((\l x. x) ((\y. y) b))` with ... result.`` () =
+        let rightRedex = Application(LambdaAbstraction('f', Variable('f')), Application(Variable('y'), Variable('z')))
+
+        let firstPart = 
+            LambdaAbstraction('x', 
+                LambdaAbstraction('y', 
+                    LambdaAbstraction('z', 
+                        Application(
+                            Application(Variable('x'), Variable('z')), rightRedex))))    
+                        
+        let secondPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
+
+        let thirdPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
+
+        Application(Application(firstPart, secondPart), thirdPart)
+        |>
+        normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))   
     
     [<Test>]
     member this.``Test should correct normalize `((\l x. \l y. x y x) b) c` with ... result.`` () =
-        Applique(
-            Applique(
-                LambdaAbstract('x', 
-                    LambdaAbstract('y', 
-                        Applique(
-                            Applique(
+        Application(
+            Application(
+                LambdaAbstraction('x', 
+                    LambdaAbstraction('y', 
+                        Application(
+                            Application(
                                 Variable('x'), Variable('y')), Variable('x')))), Variable('b')), Variable('c'))
         |>
         normalizeTerm 
-        |> should equal (Applique(Applique(Variable('b'), Variable('c')), Variable('b')))   
+        |> should equal (Application(Application(Variable('b'), Variable('c')), Variable('b')))   
     
     [<Test>]
     member this.``Big main test. Test should correct normalize `((\l x. \l y. \l z. x z (y z)) (\l x. \l y. x)) \l x. \l y. x` with ... result.`` () =
         let firstPart = 
-            LambdaAbstract('x', 
-                LambdaAbstract('y', 
-                    LambdaAbstract('z', 
-                        Applique(
-                            Applique(Variable('x'), Variable('z')), Applique(Variable('y'), Variable('z'))))))    
+            LambdaAbstraction('x', 
+                LambdaAbstraction('y', 
+                    LambdaAbstraction('z', 
+                        Application(
+                            Application(Variable('x'), Variable('z')), Application(Variable('y'), Variable('z'))))))    
                         
-        let secondPart = LambdaAbstract('x', LambdaAbstract('y', Variable('x')))
+        let secondPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
 
-        let thirdPart = LambdaAbstract('x', LambdaAbstract('y', Variable('x')))
+        let thirdPart = LambdaAbstraction('x', LambdaAbstraction('y', Variable('x')))
 
-        Applique(Applique(firstPart, secondPart), thirdPart)
+        Application(Application(firstPart, secondPart), thirdPart)
         |>
-        normalizeTerm |> should equal (LambdaAbstract('z', Variable('z')))   
+        normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))   
             
     [<Test>]
     member this.``Test should correct normalize `(\l x. x) (\l y. y)` with ... result.`` () =        
-        let term = Applique(LambdaAbstract('x', Variable('x')), LambdaAbstract('y', Variable('y')))
+        let term = Application(LambdaAbstraction('x', Variable('x')), LambdaAbstraction('y', Variable('y')))
         
-        term |> normalizeTerm |> should equal (LambdaAbstract('y', Variable('y')))
+        term |> normalizeTerm |> should equal (LambdaAbstraction('y', Variable('y')))
 
     // Test with hand alpha conversion. But this conversion is useless.
     [<Test>]
     member this.``Test should correct normalize `((\l x. \l y. \l z. x z (y z)) (\l a. \l b. a)) \l c. \l d. c` with ... result.`` () =
         let firstPart = 
-            LambdaAbstract('x', 
-                LambdaAbstract('y', 
-                    LambdaAbstract('z', 
-                        Applique(
-                            Applique(Variable('x'), Variable('z')), Applique(Variable('y'), Variable('z'))))))    
+            LambdaAbstraction('x', 
+                LambdaAbstraction('y', 
+                    LambdaAbstraction('z', 
+                        Application(
+                            Application(Variable('x'), Variable('z')), Application(Variable('y'), Variable('z'))))))    
                         
-        let secondPart = LambdaAbstract('a', LambdaAbstract('b', Variable('a')))
+        let secondPart = LambdaAbstraction('a', LambdaAbstraction('b', Variable('a')))
 
-        let thirdPart = LambdaAbstract('c', LambdaAbstract('d', Variable('c')))
+        let thirdPart = LambdaAbstraction('c', LambdaAbstraction('d', Variable('c')))
 
-        let term = Applique(Applique(firstPart, secondPart), thirdPart)
+        let term = Application(Application(firstPart, secondPart), thirdPart)
         
-        term |> normalizeTerm |> should equal (LambdaAbstract('z', Variable('z')))
+        term |> normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))
         
     // Tests for debug.
     [<Test>]
     member this.``Step 2. Test should correct normalize `some` with ... result.`` () =
         let firstPart = 
-            LambdaAbstract('y',
-                LambdaAbstract('z', 
-                    Applique(
-                        Applique(LambdaAbstract('u', LambdaAbstract('i', Variable('u'))), Variable('z')), Applique(Variable('y'), Variable('z')))))
+            LambdaAbstraction('y',
+                LambdaAbstraction('z', 
+                    Application(
+                        Application(LambdaAbstraction('u', LambdaAbstraction('i', Variable('u'))), Variable('z')), Application(Variable('y'), Variable('z')))))
         
         let secondPart = 
-            LambdaAbstract('h', 
-                LambdaAbstract('j',
+            LambdaAbstraction('h', 
+                LambdaAbstraction('j',
                     Variable('h')))
 
-        Applique(firstPart, secondPart)
+        Application(firstPart, secondPart)
         |>
-        normalizeTerm |> should equal (LambdaAbstract('z', Variable('z')))
+        normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))
 
     [<Test>]
     member this.``Step 3. Test should correct normalize `(\l z. ((\l t u. t) z)) ((\l b c. b) z))` with ... result.`` () =
-        (LambdaAbstract('z', 
-            Applique(
-                Applique(
-                    LambdaAbstract('t', 
-                        LambdaAbstract('u', 
+        (LambdaAbstraction('z', 
+            Application(
+                Application(
+                    LambdaAbstraction('t', 
+                        LambdaAbstraction('u', 
                             Variable('t'))), 
                                 Variable('z')),
-                                    Applique(
-                                        LambdaAbstract('b', 
-                                            LambdaAbstract('c', Variable('b'))), Variable('z')))))
+                                    Application(
+                                        LambdaAbstraction('b', 
+                                            LambdaAbstraction('c', Variable('b'))), Variable('z')))))
         |>
-        normalizeTerm |> should equal (LambdaAbstract('z', Variable('z')))
+        normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))
 
     [<Test>]
     member this.``Step 4. Test should correct normalize `(\l z. (\l a. z)) ((\l b c. b) z))` with ... result.`` () =
-        (LambdaAbstract('z', 
-            Applique(
-                LambdaAbstract('a', 
+        (LambdaAbstraction('z', 
+            Application(
+                LambdaAbstraction('a', 
                     Variable('z')),
-                        Applique(
-                            LambdaAbstract('b', 
-                                LambdaAbstract('c', Variable('b'))), Variable('z')))))
+                        Application(
+                            LambdaAbstraction('b', 
+                                LambdaAbstraction('c', Variable('b'))), Variable('z')))))
         |>
-        normalizeTerm |> should equal (LambdaAbstract('z', Variable('z')))
+        normalizeTerm |> should equal (LambdaAbstraction('z', Variable('z')))
     
     (*[<Test>]
     member this.``Test should correct set alphabet with ... result.`` () =
