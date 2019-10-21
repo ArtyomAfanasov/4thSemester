d1f2880decbc709cb03a93c5c51e56e688c09c60
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-10-21 18:46:02 +0300

    * Изменение модели справочника. * Рефакторинг Phonebook.fs. * Поиск телефона по имени * Вывод всей базы * Нормализация телефона, если это возможно * Тесты к этому

--- IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/Phonebook.Tests.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics.Tests/Phonebook.Tests.fs
@@ -6,7 +6,61 @@ open NUnit.Framework
 open FsUnit
 
 [<TestFixture>]
-type PhonebookTestClass () =    
+type PhonebookTestClass () =        
     [<Test>]
-    member this.``Add record (name and phone)`` () =
+    member this.``Test should add records (name and phone).`` () =        
+        let phones = addRecord "name" "phone" Map.empty
+        let updatedPhones = addRecord "name_2" "phone_2" phones
+
+        Map.find "name" phones |> should equal "phone"
+        Map.find "name_2" updatedPhones |> should equal "phone_2"
+    
+    [<Test>]
+    member this.``Test should find phone by name.`` () =
+        let phones = addRecord "name" "phone" Map.empty
+        let updatedPhones = addRecord "name_2" "phone_2" phones
+        
+        findPhoneByName "name" updatedPhones |> should equal "phone"
+        findPhoneByName "name_2" updatedPhones |> should equal "phone_2"        
+    
+    [<Test>]
+    member this.``Test should not find phone by not existed name.`` () =
+        let phones = addRecord "name" "phone" Map.empty
+        
+        findPhoneByName "notExistedName" phones |> should equal keyNotFoundError    
+    
+    [<Test>]
+    member this.``Test should not find phone in empty phonebook.`` () =
+        findPhoneByName "notExistedName" Map.empty |> should equal keyNotFoundError    
+    
+    [<Test>]
+    member this.``Test should not find name by phone.`` () =
+        let phones = addRecord "name" "phone" Map.empty
+        let updatedPhones = addRecord "name_2" "phone_2" phones
+
+        findNameByPhone "phone" updatedPhones |> should equal "name"
+        findNameByPhone "phone_2" updatedPhones |> should equal "name_2"
+    
+    [<Test>]
+    member this.``Test should not find name by not existed phone.`` () =
+        let phones = addRecord "name" "phone" Map.empty
+        findNameByPhone "notExistedPhone" phones |> should equal keyNotFoundError
+    
+    [<Test>]
+    member this.``Test should not find name in empty phonebook.`` () =
+        findNameByPhone "notExistedName" Map.empty |> should equal keyNotFoundError    
+    
+    [<Test>]
+    member this.``Test should correct normalize phone.`` () =
+        normalizePhone "+7(999)-888-77-66" |> should equal "79998887766"    
+    
+    [<Test>]
+    member this.``Test should fail on incorrect phone name while normalize.`` () =
+        (fun () -> normalizePhone "987incorrectPhone123" |> ignore)
+        |> should (throwWithMessage phoneStringError) typeof<System.Exception> 
+    
+    [<Test>]
+    member this.``Test`` () =
+        let phones = addRecord "name" "phone" Map.empty
+        let updatedPhones = addRecord "name" "phone" phones
         ()
--- IntroductionFSharp/Hw5.Generics/Hw5.Generics/Phonebook.fs
+++ IntroductionFSharp/Hw5.Generics/Hw5.Generics/Phonebook.fs
@@ -1,62 +1,116 @@
-﻿module Phonebook
+﻿/// Содержит реализацию телефонного справочника.
+module Phonebook
 
-let addRecord (name : string) (phone : string) (hashName : Map<string,string>) (hashPhone : Map<string,string>) = 
-    hashName.Add(name, phone), hashPhone.Add(phone, name)    
+/// Ошибка: "Данной записи нет в телефонной книге."
+let keyNotFoundError = "Данной записи нет в телефонной книге."
 
-let findNumberByName (name : string) (hashName : Map<string,string>) = 
-    if not <| hashName.ContainsKey(name) then "Данной записи нет в телефонной книге."
-    else hashName.Item(name)     
+/// Ошибка: "Для записи телефона доступны только символы из алфавита: { 0..9, +, -, (, ) }."
+let phoneStringError = "Для записи телефона доступны только символы из алфавита: { 0..9, +, -, (, ) }."
 
-let findNameByPhone phone =
-    ()
+/// Добавить запись (имя, телефон).
+let addRecord (name : string) (phone : string) (phoneHashTable : Map<string,string>) = 
+    phoneHashTable.Add(name, phone)    
 
-let printAll () =
-    ()
+/// Найти номер по имени.
+let findPhoneByName (name : string) (phoneHashTable : Map<string,string>) = 
+    if not <| phoneHashTable.ContainsKey(name) then keyNotFoundError
+    else phoneHashTable.Item(name)     
 
+/// Найти имя по телефону.
+let findNameByPhone (phone : string) (phoneHashTable : Map<string,string>) =
+    match Map.tryFindKey (fun name phoneElement ->  phoneElement = phone) phoneHashTable with
+    | Some(key) -> key
+    | None -> keyNotFoundError
+
+/// Вывести все пары (имя, телефон) из базы.
+let printAll phoneHashTable =
+    Map.iter (fun name phone ->
+        printfn "Владалец телефона %s --- это %s" phone name) phoneHashTable
+
+/// Сохранить данные в файл.
 let save () =
     ()
 
+/// Загрузить данные из файла.
 let load () =
     ()
 
-    // TODO
-let invitation () =
-    printfn "==========Для выхода нажмите клавишу 'q'.===========" 
-    printfn "Для выхода введите клавишу 'q'."   
+/// Нормализовать введённый номер.
+let normalizePhone (phone : string) = 
+    let normalizedCharSeq =
+        Seq.filter (fun letter -> 
+        match letter with
+        | '+' | '(' | ')' | '-' -> false
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> true
+        | _ -> failwith phoneStringError) phone
+
+    let normalizedStringSeq = 
+        Seq.map(fun letter ->
+            letter.ToString()) normalizedCharSeq
+
+    String.concat "" normalizedStringSeq
+
+/// Вывести управление.
+let printInvitation () =
+    printfn "\nДля выхода введите клавишу 'q'."   
     printfn "Для следующих операций вводите указанную клавишу."         
     printfn "1 - добавить запись (имя и телефон)"
     printfn "2 - найти телефон по имени"
     printfn "3 - найти имя по телефону"
     printfn "4 - вывести всё текущее содержимое базы"
     printfn "5 - сохранить текущие данные в файл"
-    printfn "6 - считать данные из файла"            
+    printfn "6 - считать данные из файла\n"        
 
 [<EntryPoint>]
 let main argv =
-    let rec interactiveMod hashName hashPhone = 
-        invitation ()
+    let rec interactiveMod phoneHashTable = 
+        printInvitation ()
+
         let request = System.Console.ReadLine()
         match request with 
         | "q" -> 
+            System.Console.Clear()
+            
             printfn "Произошёл выход из приложения." 
             System.Environment.Exit 0 
         | "1" -> 
+            System.Console.Clear()
+            
             printfn "Введите имя, а потом телефон через Enter."
             let name = System.Console.ReadLine()
             let phone = System.Console.ReadLine()
 
-            let updatedHashName, updatedHashPhone = addRecord name phone hashName hashPhone
-            interactiveMod updatedHashName updatedHashPhone
+            let normalizedPhone = normalizePhone phone                
+
+            let updatedPhoneHashTable = addRecord name normalizedPhone phoneHashTable
+            interactiveMod updatedPhoneHashTable
         | "2" -> 
+            System.Console.Clear()
+
             printfn "Введите имя, по которому будет производиться поиск."
             let name = System.Console.ReadLine()
-            printfn "%s" (findNumberByName name hashName)
+            printfn "%s имеет следующий телефон: %s\n\n" name (findPhoneByName name phoneHashTable)
 
-            interactiveMod hashName hashPhone
-        | _ -> interactiveMod hashName hashPhone
+            interactiveMod phoneHashTable
+        | "3" ->
+            System.Console.Clear()
+            
+            printfn "Введите телефон, по которому будет производиться поиск."
+            let phone = System.Console.ReadLine()
+            printfn "Телефон %s имеет: %s\n\n" phone (findNameByPhone phone phoneHashTable)
 
-    let hashName = Map.empty
-    let hashPhone = Map.empty
+            interactiveMod phoneHashTable
+        | "4" ->
+            System.Console.Clear()
+            
+            printfn "Всё содержимое базы телефонного справочника:"
+            
+            if phoneHashTable.Count = 0 then printfn "Телефонная база пуста.\n\n"
+            else printAll phoneHashTable               
 
-    interactiveMod hashName hashPhone
+            interactiveMod phoneHashTable
+
+        | _ -> interactiveMod phoneHashTable
+
+    interactiveMod Map.empty
     0
