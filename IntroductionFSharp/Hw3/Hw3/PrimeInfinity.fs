dd0b061014908c1899eec468f2f7d64aeda197c0
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-10-22 20:40:57 +0300

    * Оптимищация

--- IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
+++ IntroductionFSharp/Hw3/Hw3/PrimeInfinity.fs
@@ -1,24 +1,19 @@
 ﻿/// Модуль с функцией, генерирующей бесконечную последовательность простых чисел.
 module PrimeInfinity
 
+let isPrime number = 
+    let rec loop lessThanCurrentNumber =
+        (lessThanCurrentNumber > ((sqrt (number |> double)) |> int)) || 
+        (number % lessThanCurrentNumber <> 0 && loop (lessThanCurrentNumber + 1))
+    
+    loop 2
+
 /// Сгенерировать бесконечную последовательность простых чисел.
 let generatePrimeInfinity () =
     let dirtyInfinity = Seq.initInfinite (fun index ->
-        let number = index + 2
-                
-        let checkPrime number = 
-            true
-
-        //let rec loop lessThanCurrentNumber =
-        //    match lessThanCurrentNumber with
-        //    | _ when number = lessThanCurrentNumber -> true
-        //    | _ -> 
-        //        if (number % lessThanCurrentNumber = 0) then false
-        //        else loop (lessThanCurrentNumber + 1)
-
-        let isPrime = checkPrime number
+        let number = index + 2                   
         
-        if isPrime then number
+        if isPrime number then number
         else 0)
 
     Seq.choose (fun x ->
@@ -38,9 +33,8 @@ let generatePrimeInfinityByFilter () =
                 if (number % lessThanCurrentNumber = 0) then false
                 else loop (lessThanCurrentNumber + 1)
 
-        let isPrime = loop 2
         
-        if isPrime then number
+        if isPrime number then number
         else 0)
 
     Seq.filter (fun x -> x <> 0) dirtyInfinity
