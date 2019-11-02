26559cb0ec1f7e05439332498e884f30cdc1807e
ArtyomAfanasov <afanasov.artyom@gmail.com>
2019-11-02 16:25:06 +0300

    Рефакторинг SyncedLazy

--- multithreading/MyLazy/MyLazy/SyncedLazy.fs
+++ multithreading/MyLazy/MyLazy/SyncedLazy.fs
@@ -5,9 +5,6 @@
 
     /// Многопоточная реализация для lazy-объекта с синхронизацией.
     type SyncedLazy<'a> (supplier : unit -> 'a) =       
-        /// Для избавления от значения после выполнения вычисления.
-        let mutable calculation = Some(supplier)
-
         /// Результат вычисления.
         [<DefaultValue>] val mutable result : 'a
         
@@ -15,28 +12,19 @@
         let isValueCreated = ref false
 
         /// Объект для синхронизации вычисления. 
-        let lockObj = new System.Object()
-
-        /// Монитор для синхронизации вычисления. 
-        let lock (lockObj : obj) f =
-            Monitor.Enter lockObj
-            try 
-                f ()
-            finally
-                Monitor.Exit lockObj
+        let locker = new System.Object()
 
         interface ILazy<'a> with
             member this.Get () =
                 if !isValueCreated then this.result
                 else
-                    lock lockObj (fun () -> 
+                    lock locker (fun () -> 
                         if !isValueCreated then this.result
                         else                 
-                            this.result <- (calculation.Value ())
+                            this.result <- (supplier ())
                             Volatile.Write(isValueCreated, true)
                             
                             // JIT should not move next instruction before calling calculation, should it? Threrefore we do not need Volatile.Write here.
-                            calculation <- None
                             this.result)                   
                             
         /// Выполнено ли вычисление.
