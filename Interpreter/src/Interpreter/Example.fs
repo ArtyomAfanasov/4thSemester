97c31916cf91d731eed0fb6a9f9d5f2d16c56b62
ArtyomAfanasov <afanasov.artyom@gmail.com>
2020-01-08 18:13:59 +0300

    Добавлено чтение текстового файла. Добавлен текстовый файл для примера работы

--- Interpreter/src/Interpreter/Example.fs
+++ Interpreter/src/Interpreter/Example.fs
@@ -4,6 +4,7 @@ module Example
 open System
 open Interpreter
 open Normalizer
+open System.IO
 
 [<EntryPoint>]
 let main argv =
@@ -11,11 +12,16 @@ let main argv =
     // display λ chars correctly
     Console.OutputEncoding <- Text.Encoding.Unicode
 
-    let myOwn = 
-        "(lx.ly.(x y) b)"
-            |> Parse.parse
-    myOwn 
-    |> normalizeTerm
-    |> printfn "%A"
+    let readLines = seq {
+        use sr = new StreamReader ("../../../../../tests/Input/input.txt")
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
+
+    Seq.iter (fun term ->
+        printfn "Не нормализованный терм: %s" term
+        term |> Parse.parse |> normalizeTerm |> 
+        printfn "Нормализованный терм: %A\n"
+        ) readLines
 
     0
